d66c9d13c750e402177571b102964ae256031a34
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-08-23 12:38:58 +0600

    redux basit add korlam

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Home from "./pages/Home"
 
 
 function App() {
 
   return (
     <>
-    <h1 className="bg-slate-800 text-xl text-fuchsia-50 font-bold">Hello Redux </h1>
+<Home></Home>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+                <h1 className="bg-slate-800 text-xl text-fuchsia-50 font-bold">Hello Redux </h1>
+
+        </div>
+    );
+};
+
+export default Home;
