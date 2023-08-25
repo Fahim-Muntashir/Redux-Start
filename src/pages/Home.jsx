da16e1e8425ab69d66d5887b4eb6e57a38739a31
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-08-25 09:22:33 +0600

    Consuming the state, action dispatch, and payload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
+import Counter from "../components/Counter";
 
 const Home = () => {
     return (
         <div>
                 <h1 className="bg-slate-800 text-xl text-fuchsia-50 font-bold">Hello Redux </h1>
-
+<Counter></Counter>
         </div>
     );
 };
